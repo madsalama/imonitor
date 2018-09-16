5e1b95480e80b9a1c2df8b4255f904b9b10c7f08
Mahmoud Salama <msalama@localhost.localdomain>
2018-09-15 20:17:02 -0400

    $message

--- push.sh
+++ push.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-git add .
-git commit -a -m 'commit'
-git push
+
+if [ -z $1 ]
+then
+message='commit'
+elif [ -z $2 ]
+then
+branch='master'
+fi 
+
+#message=$1
+#branch=$2
+
+git commit -a -m '$message'
+git push origin $branch 
